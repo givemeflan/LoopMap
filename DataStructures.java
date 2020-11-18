f5a110ddf419e81bb6b1eec1389fecd9a39ea8fa
Laura Le <lle387@gmail.com>
2020-11-18 18:07:06 -0500

    putting instructions down

--- DataStructures.java
+++ DataStructures.java
@@ -1,5 +1,20 @@
-// public class DataStructures {
+public class DataStructures {
 
-// public static void main(String[] args){
+public static void main(String[] args){
+    double[ ] exampleArray = {1,5,6,5,4,1};
+    double maximum = examplesArray[0];
 
-// }
+    int index = 0;
+
+    for (int i = 1; i<exampleArray.length>; i++){
+
+         if (exampleArray[ i ] > maximum) {
+
+              maximum = exampleArray[ i ];
+
+              index = i;
+
+         }
+
+    }
+}
