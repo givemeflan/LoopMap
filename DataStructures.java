a632e21e18033ba7439a4ad8537ccb6552b16e6e
Laura Le <lle387@gmail.com>
2020-11-17 22:08:16 -0500

    slowly adding first section of code

---
+++ DataStructures.java
@@ -0,0 +1,5 @@
+public class DataStructures {
+
+    public static void main(String[] args){ 
+
+}
--- LoopandMap.java
+++ LoopandMap.java
@@ -6,9 +6,17 @@ import java.util.Scanner;
 public class LoopandMap {
 
     public static void main(String[] args){
+        // This Section is for asking user for 5 numbers
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Can you give me five numbers?");
+        int userNum = scan.nextInt(); 
 
+
+
+        // This section is for the HashMap
         HashMap<String, String> cars = new HashMap<String, String>();
 
+        // Adding elements to the HashMap
         cars.put("Lexus", "LFA");
         cars.put("Audi", "R8");
         cars.put("Jaguar", "iPace");
@@ -16,15 +24,13 @@ public class LoopandMap {
 
         Scanner scan= new Scanner(System.in);
         System.out.println("What kind of make do you want?");
-        String desiredCar = scan.nextLine();
+        String wantedCar = scan.nextLine();
 
         if (cars.containsKey(desiredCar)) {
-            (System.out.println("We have a " + desiredCar + "" cars.get(desiredCar));
+            (System.out.println("We have a " + wantedCar + "" cars.get(wantedCar));
             else{
                 System.out.println("We don't have that car unfortunately");
             }
-
-}
 
 }
 }
